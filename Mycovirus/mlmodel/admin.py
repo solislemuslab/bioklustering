e43e4b9fa1e72078c29da794fae13f912498cb31
chunrong huang <chuang278@wisc.edu>
2020-12-10 11:15:54 -0600

    remove old repo

--- Mycovirus/mlmodel/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import FileInfo, FileListInfo
-
-admin.site.register(FileInfo)
-admin.site.register(FileListInfo)
--- Mycovirus/mlmodel/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class MlmodelConfig(AppConfig):
-    name = 'mlmodel'
