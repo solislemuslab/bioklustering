8f448db6b076b556ba932f8e00031b629cba2d4c
chunrong huang <chuang278@wisc.edu>
2021-01-26 02:05:22 -0600

    add login tests

--- BioKlustering-Website/mlmodel/tests.py
+++ BioKlustering-Website/mlmodel/tests.py
@@ -1,3 +1,22 @@
+import unittest
+from selenium import webdriver
 from django.test import TestCase
 
-# Create your tests here.
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        # a local path that has your downloaded chromedriver.exe
+        PATH = '/Users/hcr/Downloads/chromedriver'
+        self.driver = webdriver.Chrome(PATH)
+
+    def test_login_fire(self):
+        self.driver.get("http://127.0.0.1:8000/accounts/login")
+        self.driver.find_element_by_id('id_username').send_keys("user1")
+        self.driver.find_element_by_id('id_password').send_keys("test-user1-pwd")
+        self.driver.find_element_by_id('login_button').click()
+        self.assertIn("http://127.0.0.1:8000/", self.driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    if __name__ == '__main__':
+        unittest.main()
