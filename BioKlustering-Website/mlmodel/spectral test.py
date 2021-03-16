697a15fdbb0ee996f0ed2f4e85af0028b433053a
Yangliule <43895605+Yangliule@users.noreply.github.com>
2021-03-17 01:05:40 +0800

    Add files via upload

---
+++ BioKlustering-Website/mlmodel/spectral test.py
@@ -0,0 +1,53 @@
+import spectralClustering
+import sys
+
+sys.path.append(r'parser/')
+
+
+def test_spectral_Unsup(test_file_1):
+    k_min = 2
+    k_max = 3
+    num_class = 2
+
+    [labels], [plot] = spectral_clustering(0, test_file_1, k_min, k_max, num_class, "kmeans", "PCA")
+
+    assert all(labels == [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+       1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1,
+       1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+       1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0,
+       1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+       1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+       1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+       1, 1])
+
+
+def test_spectral_semi(test_file_1, test_file_2):
+    k_min = 2
+    k_max = 3
+    num_class = 2
+    cov_type = 'full'
+    seed = 699
+
+    [labels], [plot] = intuitive_semi_supervised(0, test_file_1, test_file_2, k_min, k_max, num_class, "discretize", seed, 'PCA')
+
+    assert all(labels == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+       1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1,
+       0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1,
+       1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+       1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+       1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0,
+       1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0,
+       1, 1])
+
+
+
+
+test_file_1 = "../../manuscript/validation_data/Semi-supervised-test-dataset/combined_Bat_Cat_flu.fa"  # need to be changed to filepath in github
+test_file_2 = '../../manuscript/validation_data/Semi-supervised-test-dataset/labels_ten_percent_only0s.csv'
+test_spectral_Unsup(test_file_1)
+test_spectral_semi(test_file_1, test_file_2)
+
+print("Everything passed")
+
